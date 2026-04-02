ab1bcf5f34b0610cab7cb5a97c4c8762bd993156
Syntax <kirbypaladan24@gmail.com>
2026-04-02 17:11:16 +0800

    Update roles.js

--- backend/src/utils/roles.js
+++ backend/src/utils/roles.js
@@ -6,10 +6,23 @@ export const ROLE = {
   SUPER_ADMIN: "super_admin",
 };
 
-export const ADMIN_ROLES = [
+export const PRIMARY_SUPER_ADMIN_UID = "6bs7TaQnJBZDGiyhR1eoDMLncsb2";
+
+export const DEFAULT_ADMIN_ROLE_BY_UID = new Map([
+  [PRIMARY_SUPER_ADMIN_UID, ROLE.SUPER_ADMIN],
+  ["7gquSWQ94xZZLMxLCW4Xlv2QJ613", ROLE.CONTENT_ADMIN],
+  ["L6aGCzr08Wd4gcj6ndiAqa0Z5dx2", ROLE.LANDMARK_ADMIN],
+  ["TI0yeuCaYcggEJmjh7H4BlAmp562", ROLE.EMERGENCY_ADMIN],
+]);
+
+export const MANAGED_ADMIN_ROLES = [
   ROLE.CONTENT_ADMIN,
   ROLE.LANDMARK_ADMIN,
   ROLE.EMERGENCY_ADMIN,
+];
+
+export const ADMIN_ROLES = [
+  ...MANAGED_ADMIN_ROLES,
   ROLE.SUPER_ADMIN,
 ];
 
